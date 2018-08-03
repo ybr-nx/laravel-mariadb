9fce15f0e42430bd8476360dc9d226579c1be728
Ingmar Aasoja <ingmar@zone.ee>
2018-08-03 14:01:11 +0300

    Fixes #4

--- src/MariaDBServiceProvider.php
+++ src/MariaDBServiceProvider.php
@@ -8,10 +8,10 @@ class MariaDBServiceProvider extends ServiceProvider
 {
     public function register()
     {
-        $app = $this->app;
-
-        $app['db']->extend('mariadb', function ($config) use ($app) {
-            return (new ConnectionFactory($app))->make($config);
+        $this->app->resolving('db', function ($db, $app) {
+            $db->extend('mariadb', function ($config, $name) use ($app){
+                return (new ConnectionFactory($app))->make($config, $name);
+            });
         });
     }
 }
