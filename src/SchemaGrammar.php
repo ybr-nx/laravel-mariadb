1b86dab25418bd3381032d5adb2bfdaf078e7cc4
Ingmar Aasoja <ingmar@aasoja.ee>
2018-08-17 21:58:35 +0300

    Fixes #6 - Add 'Check' modifier before 'After'

--- src/SchemaGrammar.php
+++ src/SchemaGrammar.php
@@ -11,9 +11,14 @@ class SchemaGrammar extends MySqlGrammar
 
     public function __construct()
     {
-    	if (!in_array('Check', $this->modifiers)) {
-    		$this->modifiers[] = 'Check';
-    	}
+        if (!in_array('Check', $this->modifiers)) {
+            array_splice(
+                $this->modifiers,
+                array_search('After', $this->modifiers),
+                count($this->modifiers),
+                array_merge(['Check'], array_slice($this->modifiers, array_search('After', $this->modifiers)))
+            );
+        }
     }
 
     protected function modifyCheck(Blueprint $blueprint, Fluent $column)
